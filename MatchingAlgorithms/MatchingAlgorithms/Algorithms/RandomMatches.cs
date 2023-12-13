16bc1d93d1afba25c674ed90ddb30c66ef666ef8
Sietse <49184286+Sakeun@users.noreply.github.com>
2023-12-13 11:20:09 +0100

    RandomMatching algorithm
    algorithm that gets random vectors from based on matching keywords, without weight

---
+++ MatchingAlgorithms/MatchingAlgorithms/Algorithms/RandomMatches.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+
+namespace MatchingAlgorithms;
+
+/*
+ This class contains a really simple algorithm that selects 3 random apps based on if they have any matching words
+ It does not take any weight of amounts matching into account
+*/
+public class RandomMatches
+{
+    public string[] RandomMatch(Vector[] app, Vector[] installed)
+    {
+        var matchingWords = new List<string>();
+        
+        foreach (var a in installed)
+        {
+            foreach (var keyW in a.Keywords)
+            {
+                if (!matchingWords.Contains(keyW))
+                {
+                    matchingWords.Add(keyW);
+                }
+            }
+        }
+
+        var apps = new List<string>();
+        foreach (var a in app)
+        {
+            if (matchingWords.Exists(word => a.Keywords.Contains(word)))
+            {
+                apps.Add(a.Name);
+            }
+        }
+
+        var randomNums = GenerateNumbers(apps);
+
+
+        return new []{apps[randomNums[0]], apps[randomNums[1]], apps[randomNums[2]]};
+    }
+    
+    private int[] GenerateNumbers(ICollection arr)
+    {        
+        Random rand = new Random();
+        var randomNums = new int[3];
+
+        for (int i = 0; i < 3; i++)
+        {
+            var j = rand.Next(0, arr.Count);
+            while(randomNums.Contains(j))
+            {
+                j = rand.Next(0, arr.Count);
+            }
+
+            randomNums[i] = j;
+        }
+
+        return randomNums;
+    }
+}
---
+++ MatchingAlgorithms/MatchingAlgorithms/Models/Vector.cs
@@ -0,0 +1,7 @@
+namespace MatchingAlgorithms;
+
+public class Vector
+{
+    public string Name { get; set; }
+    public string[] Keywords { get; set; }
+}
